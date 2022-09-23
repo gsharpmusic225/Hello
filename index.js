87b3d70f3e9a71c4d391f83dbde84ace792eac41
Gerren Porch <gerrenporch@Gerrens-MBP.attlocal.net>
2022-09-22 21:36:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const sayHelloTo = (name = "World") => console.log(`Hello ${name}`)
+sayHelloTo("Gerren")
+
+const add = (numb1, numb2) => numb1 + numb2
+const name = "Gerren"
+const numb1 = 5
+const numb2 = 6475
+console.log(add (numb1, numb2))
